38bf140b0e300f917cefc6a2beed8cbf9bdf8f32
Chandra Abdul Fattah <chandrashibezzo@gmail.com>
2022-07-25 23:42:00 +0700

    Add desktop platform

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_avo_inspector
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterAvoInspectorPlugin.register(with: registry.registrar(forPlugin: "FlutterAvoInspectorPlugin"))
+}
---
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <flutter_avo_inspector/flutter_avo_inspector_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  FlutterAvoInspectorPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterAvoInspectorPlugin"));
+}
